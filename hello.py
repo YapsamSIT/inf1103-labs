edf3fbc926ac1000b52cbaaf2c4f6c521a4682cd
Sam <yapdongsam@gmail.com>
2026-09-04 11:19:26 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -22,15 +22,15 @@ print("Day 2:", followers)
 followers += 10
 print("Day 3:", followers)
 
-# username = input("Enter Username: ")
-# age = input("Enter Age: ")
-# category = input("Enter Content Category: ")
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-# print("\nInstagram Profile")
-# print("=============================")
-# print("Username:", username)
-# print("Age:", age)
-# print("Category:", category)
+print("\nInstagram Profile")
+print("=============================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
 
 # if age>40 and category == "fun":
 #     print
